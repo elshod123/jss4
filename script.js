e295c2909424e43ea58c3fe3a0d858b249550b7c
user123456 <elshodtashkeniski@gmail.com>
2025-07-02 17:04:09 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+let a = prompt('your name')
+let b = prompt('harf yozing')
+
+
+
+if(a.toLowerCase().includes(b.toLowerCase())){
+    alert("bu xarf bor")
+}else{
+    alert("bu xarf yoq")
+}
+
+
+
+
+
